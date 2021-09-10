b1b80db64c4f393bed93ec4b4ff2c22cbb9dddd8
stellamoraes <st.francismoraes@gmail.com>
2021-09-10 17:02:41 -0300

    Estruturas sequenciais (python brasil)

---
+++ exer_estr_seq.py
@@ -0,0 +1,30 @@
+'''
+#tarefa 1
+print ('Olá, mundo!')
+#tarefa 2
+n= input ('Insira um número:')
+print ('O número informado foi ' + n)
+#tarefa 3
+a = input ('Insira um número:')
+b = input ('Insira um número:')
+print a + b
+#tarefa 4
+a= int(input('Insira a primeira nota:'))
+b= int(input('Insira a segunda nota:'))
+c= int(input('Insira a terceira nota:'))
+d= int(input('Insira a quarta nota:'))
+print ('A média das notas é: ', ((a + b + c + d)/ 4))
+#tarefa 5
+med_metro = int(input('Insira o valor  em (m) a ser convertido para (cm):'))
+conversao= med_metro/100
+print (conversao)
+'''
+#tarefa 6
+raio = int(input ('Raio do circunferência:'))
+calculo= 3.14*raio**2 
+print (calculo)
+#tarefa 7
+l= int(input('Lado do quadrado:'))
+area= l**2 
+db_area= area * 2
+print (db_area)
