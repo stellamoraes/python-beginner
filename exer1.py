0d4f044a1f4bfb740dc28ec48c495b9364dc7be3
stellamoraes <st.francismoraes@gmail.com>
2021-09-10 16:35:09 -0300

    exer1:  atualizar string

--- exer1.py
+++ exer1.py
@@ -4,7 +4,7 @@ count= int(input('Quantidade de donuts:'))
 def donuts (count):
     returno = ""
     if count < 10:
-        returno = "Número de donuts: <{}>".format(count)
+        returno = "Número de donuts: <{}> ".format(count)
     else:
         returno = "Número de donuts: many"        
     return returno
