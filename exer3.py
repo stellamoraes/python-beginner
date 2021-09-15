fc3f15a08de782068104f25772f6a9ad07d91416
Stella Francis Moraes <80214673+stellamoraes@users.noreply.github.com>
2021-09-15 14:42:00 -0300

    Delete exer3.py

--- exer3.py
+++
@@ -1,9 +0,0 @@
-palavra= input('Insira uma palavra:')
-
-def fix_start(palavra): 
-    letra_um= palavra[0]
-    resto_palavra= palavra[1:]
-    nova_palavra= resto_palavra.replace(palavra[0],'*') 
-    return (letra_um) + (nova_palavra)
-fix_start(palavra)
-print (fix_start(palavra))
